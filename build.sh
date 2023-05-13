ab6af292ebf2ceff087c82abecf60806519335df
Liu Y Cheng <xsro@foxmail.com>
2023-05-13 17:18:50 +0800

    update build script

--- build.sh
+++ build.sh
@@ -2,6 +2,13 @@ cargo build --release
 cp target/release/typst*.exe addons/vscode/out/
 cp target/release/typst* addons/vscode/out/
 cd addons/vscode
+mv README.md ../README.md.bak
+echo "# gitee.com/xsro/typst-lsp 修改版\n" >> README.md
+date >> README.md
+echo ``` >>README.md
+git status >>README.md
+echo ``` >>README.md
+cat ../README.md.bak >> README.md
 npm run package
 rm out/*.*
 ver="0.4.1"
@@ -14,3 +21,5 @@ case "$OSTYPE" in
   *)        platform="unknown: $OSTYPE" ;;
 esac
 mv typst-lsp-$ver.vsix typst-lsp-${ver}_$platform-$(arch).vsix
+rm README.md
+mv  ../README.md.bak README.md
